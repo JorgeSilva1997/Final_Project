ef14066e8da373610e56f9f4f6a49dd55530c01a
Jorge Silva <jorgemanuelsilva97@gmail.com>
2019-12-05 18:39:12 +0000

    Md5 ✓CHECK✓

--- app/src/main/java/com/example/finalproject/Login.java
+++ app/src/main/java/com/example/finalproject/Login.java
@@ -9,6 +9,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.math.BigInteger;
+import java.util.regex.Pattern;
+
 public class Login extends AppCompatActivity {
 
     @Override
@@ -38,5 +41,50 @@ public class Login extends AppCompatActivity {
                 }
             }
         });
+    }
+
+
+
+
+
+
+
+    // Código para verificar se a PASSWORD cumpre os requisitos (Usado no Regist.java)
+    public Boolean checkPass(String password)
+    {
+        Pattern PASSWORD_PATTERN = Pattern.compile("^" +
+                "(?=.*[0-9])" +
+                "(?=.*[a-z])" +
+                "(?=.*[A-Z])" +
+                "(?=.*[@#$%^&+=_])" +
+                "(?=\\S+$)" +
+                ".{6,12}" +
+                "$");
+        if (!PASSWORD_PATTERN.matcher(password).matches())
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+
+
+    // Encriptação da PASSWORD
+    public String computeMD5Hash(String password)
+    {
+        byte[] md5Input = password.getBytes();
+        BigInteger md5Data = null;
+        try
+        {
+            md5Data = new BigInteger(1, md5.encryptMD5(md5Input));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String md5Str = md5Data.toString(16);
+
+        return md5Str;
     }
 }
