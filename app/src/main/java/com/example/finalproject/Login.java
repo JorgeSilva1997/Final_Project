b1f7b750c45e55f2071cfcd2a751c1228d6132cc
Jorge Silva <jorgemanuelsilva97@gmail.com>
2019-12-12 17:57:17 +0000

    Eu dou te tudo menino

--- app/src/main/java/com/example/finalproject/Login.java
+++ app/src/main/java/com/example/finalproject/Login.java
@@ -41,68 +41,64 @@ public class Login extends AppCompatActivity {
 
     public void btnLogin(View view)
     {
-        final EditText name = (EditText)findViewById(R.id.user);
-        final EditText pass = (EditText)findViewById(R.id.pass);
+        EditText name = (EditText)findViewById(R.id.user);
+        EditText pass = (EditText)findViewById(R.id.pass);
 
-        final String nome = name.getText().toString();
-        final String password = pass.getText().toString();
+        String nome = name.getText().toString();
+        String password = pass.getText().toString();
         Button login = (Button)findViewById(R.id.btnLogin);
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        if (!nome.isEmpty() && !password.isEmpty())
+        {
+            String url = prefix_url + "/user/login";
+            Map<String, String> jsonParams = new HashMap<String, String>();
+            jsonParams.put("nome", nome);
+            jsonParams.put("password", password);
 
-                if (!nome.isEmpty() && !password.isEmpty())
-                {
-                    String url = prefix_url + "/user/login";
-                    Map<String, String> jsonParams = new HashMap<String, String>();
-
-                    jsonParams.put("nome", nome);
-                    jsonParams.put("password", password);
-
-                    JsonObjectRequest postRequest = new JsonObjectRequest(Request.Method.POST, url,
-                            new JSONObject(jsonParams),
-                            new Response.Listener<JSONObject>() {
-                                @Override
-                                public void onResponse(JSONObject response) {
-                                    try {
-                                        boolean status = response.getBoolean("status");
-                                        Toast.makeText(Login.this, "" + status, Toast.LENGTH_SHORT).show();
-                                        if (status) {
-                                            getUserId(nome, password);
-                                        } else {
-                                            Toast.makeText(Login.this, "Tenta outra vez", Toast.LENGTH_SHORT).show();
-                                            id_string = null;
-                                        }
-                                    } catch (JSONException ex) {
-                                        Log.d("login", "" + ex);
-                                    }
-                                }
-                            },
-                            new Response.ErrorListener() {
-                                @Override
-                                public void onErrorResponse(VolleyError error) {
-                                    Toast.makeText(Login.this, "erro", Toast.LENGTH_SHORT).show();
-                                }
-                            }) {
+            JsonObjectRequest postRequest = new JsonObjectRequest( Request.Method.POST, url,
+                    new JSONObject(jsonParams),
+                    new Response.Listener<JSONObject>() {
                         @Override
-                        public Map<String, String> getHeaders() throws AuthFailureError {
-                            HashMap<String, String> headers = new HashMap<String, String>();
-                            headers.put("Content-Type", "application/json; charset=utf-8");
-                            headers.put("User-agent", System.getProperty("http.agent"));
-                            return headers;
+                        public void onResponse(JSONObject response) {
+                            try {
+                                boolean status = response.getBoolean("status");
+                                Toast.makeText(Login.this, "" + status, Toast.LENGTH_SHORT).show();
+                                if (status) {
+                                    Intent intent = new Intent(Login.this, Main.class);
+                                    intent.putExtra("ID", response.getString("id"));
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(Login.this, "Tenta outra vez", Toast.LENGTH_SHORT).show();
+                                    id_string = null;
+                                }
+                            } catch (JSONException ex) {
+                                Log.d("login", "" + ex);
+                            }
                         }
-                    };
-                    MySingleton.getInstance(Login.this).addToRequestQueue(postRequest);
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(Login.this, "erro", Toast.LENGTH_SHORT).show();
+                        }
+                    }) {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    HashMap<String, String> headers = new HashMap<String, String>();
+                    headers.put("Content-Type", "application/json; charset=utf-8");
+                    headers.put("User-agent", System.getProperty("http.agent"));
+                    return headers;
+                }
+            };
+            MySingleton.getInstance(Login.this).addToRequestQueue(postRequest);
 
 
-                }
-                else
-                {
-                    Toast.makeText(Login.this, "Preencha os campos.", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        }
+        else
+        {
+            Toast.makeText(Login.this, "Preencha os campos.", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     public void getUserId(String nome, String password)  {
